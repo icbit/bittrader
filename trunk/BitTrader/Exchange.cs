1eb9b96cc5387253727c0e2504fcdd277c99019f
icbitse@gmail.com <icbitse@gmail.com@55366ed6-931f-05c8-9f87-702452f4f6e6>
2011-08-06 09:11:39 +0000

    - Committing an initial version. Pretty much a stub right now, which could get balance using MtGOX API and nothing more than that.

---
+++ trunk/BitTrader/Exchange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BitTrader
+{
+    class Exchange
+    {
+        protected double[] balance;
+
+        public string UserName;
+        public string Password;
+
+        virtual public void Connect()
+        {
+        }
+
+        virtual public void Disconnect()
+        {
+        }
+
+        virtual public void UpdateBalance()
+        {
+        }
+
+        public double GetBalance(int i)
+        {
+            return balance[i];
+        }
+    }
+}
